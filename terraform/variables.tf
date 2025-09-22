0d0784ccc715a8620176cd756f1a17f4bfd6fb38
Suchiudhaya <gratitudepourtout@gmail.com>
2025-09-22 12:23:50 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,34 @@
-variable "aws_region" { type = string  default = "us-east-1" }
-variable "project_name" { type = string default = "demo-app" }
-variable "instance_type" { type = string default = "t3.micro" }
-variable "key_name" { type = string default = "my-key" }
-variable "public_key_path" { type = string default = "~/.ssh/id_rsa.pub" }
-variable "git_repo" { type = string default = "" }
-variable "my_ip_cidr" { type = string default = "0.0.0.0/0" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "TerraformApp"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type    = string
+  default = "my-key"
+}
+
+variable "public_key_path" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "git_repo" {
+  type    = string
+  default = ""
+}
+
+variable "my_ip_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
