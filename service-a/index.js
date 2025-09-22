b1e41164296d0aa4a62bb7e70a288d3fdee00dd1
Suchiudhaya <gratitudepourtout@gmail.com>
2025-09-22 15:42:17 +0100

    Update index.js

--- service-a/index.js
+++ service-a/index.js
@@ -1,1 +1,11 @@
-<contents from document service-a/index.js>
+const express = require('express');
+const app = express();
+const port = 3001;
+
+
+app.get('/api/service-a', (req, res) => {
+res.json({ service: 'A', ts: new Date().toISOString() });
+});
+
+
+app.listen(port, () => console.log(`service-a listening on ${port}`));
